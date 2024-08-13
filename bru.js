b6074dbb39e474a7e646c0c7976125d542009b5b
shaik1014 <shaikizaaz00@gmail.com>
2024-08-13 18:00:42 +0530

    Add files via upload

---
+++ bru.js
@@ -0,0 +1,28 @@
+function btn1(){
+    document.getElementById("btn1").style.display='none'
+    document.getElementById("btn2").style.display='block'
+document.getElementById("btn3").style.display='block'
+document.getElementById("btn4").style.display='block'
+document.getElementById("btn5").style.display='block'
+document.getElementById("btn6").style.display='block'
+
+    
+    
+    
+}
+
+
+function btn2(){
+    document.getElementById("btn1").style.display='block'
+    document.getElementById("btn2").style.display='none'
+document.getElementById("btn3").style.display='none'
+document.getElementById("btn4").style.display='none'
+document.getElementById("btn5").style.display='none'
+document.getElementById("btn6").style.display='none'
+
+    
+    
+    
+}
+
+
